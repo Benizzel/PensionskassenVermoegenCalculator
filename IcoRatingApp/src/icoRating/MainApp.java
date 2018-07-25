d4a72b2834bbc647aafd5ca1f9c420c701012f58
beniw <beniw@LAPTOP-AKAFORD9.fritz.box>
2018-07-25 11:36:55 +0200

    Formating Changes and Bugfix
    Changed check if file exists to "file.exists" instead of "file != null"
    so that a useful info message is shown instead of a nested exception
    while unmarshaling.
    
    handleOpen() now asks for saving the current portfolio
    
    renamed isInputValid to isPortfolioValid
    
    Formated the code

--- IcoRatingApp/src/icoRating/MainApp.java
+++ IcoRatingApp/src/icoRating/MainApp.java
@@ -1,9 +1,7 @@
 package icoRating;
 
-import java.beans.EventHandler;
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
 import java.util.prefs.Preferences;
 
 import javax.xml.bind.JAXBContext;
@@ -12,7 +10,6 @@ import javax.xml.bind.Unmarshaller;
 
 import icoRating.model.Criteria;
 import icoRating.model.Ico;
-import icoRating.model.IcoCriteria;
 import icoRating.model.PortfolioWrapper;
 import icoRating.view.IcoDialogController;
 import icoRating.view.OverviewController;
@@ -24,43 +21,35 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 /**
- * Start the ico rating app
- * @author BeniW
+ * Start the IcoRatingApp 
+ * @author Benjamin Wyss
  */
-
-
 public class MainApp extends Application {
 	
-	/**
-	 * Variables
-	 */
-
-    private Stage primaryStage;
+	private Stage primaryStage;
     private BorderPane rootLayout;
-    
-    /**
-     * The data as an observable list of ICOs and Criteria. Variable Typ ist ObservableList (JavaFX Klasse) vom Typ "Ico" (meine Klasse)
-     */
-    //private ObservableList<Ico> icoData = FXCollections.observableArrayList();
     private ObservableList<Criteria> criteriaList = FXCollections.observableArrayList();
     private ObservableList<Ico> icoList = FXCollections.observableArrayList();
- //   private ObservableList<IcoCriteria> icoCriterionList = FXCollections.observableArrayList();
-    private Ico ico;
     
     /**
-     * Constructor
+     * launches the app
+     * @param args
+     */
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    /**
+     * Default Constructor
      */
     public MainApp() {
     }
@@ -81,6 +70,10 @@ public class MainApp extends Application {
         	return icoList;
     }
        	
+    /**
+     * Implements Application start and sets the App Icon
+     * @param primaryStage Stage
+     */
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -92,7 +85,8 @@ public class MainApp extends Application {
     
     /**
      * Initializes the root layout and tries to load the last opened
-     * person file.
+     * portfolio file.
+     * @throws Info Alert if last opened file cannot be found
      */
     public void initRootLayout() {
         try {
@@ -119,13 +113,24 @@ public class MainApp extends Application {
 
         // Try to load last opened person file.
         File file = getFilePath();
-        if (file != null) {
+        if (file.exists()) {
             loadCriteriaDataFromFile(file);
+        } else {
+        	Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setTitle("Portfolio");
+    		alert.setHeaderText("Could not find last Portfolio");
+    		alert.setContentText(
+        		"Could not find a Portfolio in \n" +
+        		file.getPath() + "\n\n" +
+        		"A new Portfolio will be created.\n\n" +
+        		"If you want to open an existing Portfolio use Menu 'File'->'Open'\n\n"
+    		); 
+    		alert.showAndWait();
         }
     }
     
     /**
-     * Shows the person overview inside the root layout.
+     * Shows the Ico overview inside the root layout.
      */
     public void showIcoOverview() {
     	try {
@@ -148,7 +153,9 @@ public class MainApp extends Application {
     
    
     /**
-     * ICO kommt von OverviewController
+     * Shows the Ico Dialog for edit an existing Ico or adding a new
+     * @param ico Ico to be edited or new instanced Ico
+     * @return true when ok clicked 
      */
     public boolean showIcoDialog (Ico ico) {
     	try {
@@ -167,7 +174,6 @@ public class MainApp extends Application {
     		dialogStage.setScene(scene);
     		
     		//Show the dialog Stage and set controller
-    		//TODO: Was macht controller, was macht stege etc....
     		IcoDialogController controller = loader.getController();
     		controller.setDialogStage(dialogStage);
     		controller.initialize(ico);
@@ -185,8 +191,11 @@ public class MainApp extends Application {
     	}
     } 
     
-   
-    
+    /**
+     * Shows the Criteria Dialog for edit an existing Ico or adding a new
+     * @param criteria Criteria to be edited or new instanced Criteria
+     * @return true when ok clicked 
+     */
     
     public boolean showCriteriaDialog (Criteria criteria) {
     	try {
@@ -205,7 +214,6 @@ public class MainApp extends Application {
     		dialogStage.setScene(scene);
     		
     		//Show the dialog Stage and set controller
-    		//TODO: Was macht controller, was macht stege etc....
     		CriteriaDialogController controller = loader.getController();
     		controller.setDialogStage(dialogStage);
     		controller.initialize(criteria);
@@ -223,23 +231,17 @@ public class MainApp extends Application {
     }    
 
     /**
-     * Returns the main stage.
-     * @return
+     * @return	the main stage
      */
     public Stage getPrimaryStage() {
         return primaryStage;
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
     /**
-     * Returns the person file preference, i.e. the file that was last opened.
+     * Returns the file preference, i.e. the file that was last opened.
      * The preference is read from the OS specific registry. If no such
      * preference can be found, null is returned.
-     * 
-     * @return
+     * @return file preference or null
      */
     public File getFilePath() {
         Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
@@ -272,93 +274,63 @@ public class MainApp extends Application {
         }
     }
     
-            /**
-		     * Loads criteria data from the specified file. The current criteria data will
-		     * be replaced.
-		     * 
-		     * @param file
-		     */
-		    public void loadCriteriaDataFromFile(File file) {
-		        try {
-		            JAXBContext context = JAXBContext
-		                    .newInstance(PortfolioWrapper.class);
-		            Unmarshaller um = context.createUnmarshaller();
-		
-		            // Reading XML from the file and unmarshalling.
-		            PortfolioWrapper wrapper = (PortfolioWrapper) um.unmarshal(file);
-		
-		            criteriaList.clear();
-		            criteriaList.addAll(wrapper.getCriterion());
-		            
-		            icoList.clear();
-		            icoList.addAll(wrapper.getIcos());
-		            
-//		            icoCriterionList.addAll(wrapper.getIcoCriterion());
-		            
-//		            icoList.forEach(ico -> ico.getAllIcoCriteria().clear());
-//		            icoList.forEach(ico -> {
-//		            	for (IcoCriteria c : icoCriterionList) {
-//		            		if (c.getIco() == ico) {
-//		            			ico.addCriteria(c);
-//		            		}
-//		            	}
-//		            });
-		            
-		
-		            // Save the file path to the registry.
-		            setFilePath(file);
-		
-		        } catch (Exception e) { // catches ANY exception
-		            Alert alert = new Alert(AlertType.WARNING);
-		            alert.setTitle("Warning");
-		            alert.setHeaderText("Could not load data");
-		            alert.setContentText(
-		            		"Could not load data from file:\n" +
-		            		file.getPath() + "\n\n" +
-		            		"A new Portfolio will be created.\n\n" +
-		            		"If you want to open an existing Portfolio use Menu 'File' -> 'Open'\n" +
-		            		e
-		            ); 
-		            alert.showAndWait();
-		        }
-		    }
+	/**
+	 * Loads criteria data from the specified file. The current criteria data will
+	 * be replaced.
+	 * @param file
+	 */
+	public void loadCriteriaDataFromFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PortfolioWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
 
-            /**
-		     * Saves the current person data to the specified file.
-		     * 
-		     * @param file
-		     */
-		    public void saveCriteriaDataToFile(File file) {
-		        try {
-		            JAXBContext context = JAXBContext
-		                    .newInstance(PortfolioWrapper.class);
-		            Marshaller m = context.createMarshaller();
-		            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		
-		            // Wrapping our criteria data.
-		            PortfolioWrapper wrapper = new PortfolioWrapper();
-		            wrapper.setCriterion(criteriaList);
-		            wrapper.setIcos(icoList);
-//		            icoList.forEach(ico -> {
-//		            	for (IcoCriteria c : ico.getAllIcoCriteria()) {
-//		            		icoCriterionList.add(c);
-//		            	}
-//		            });
-//		            wrapper.setIcoCriterion(icoCriterionList);
-		            
-		
-		            // Marshalling and saving XML to the file.
-		            m.marshal(wrapper, file);
-		
-		            // Save the file path to the registry.
-		            setFilePath(file);
-		        } catch (Exception e) { // catches ANY exception
-		            Alert alert = new Alert(AlertType.ERROR);
-		            alert.setTitle("Error");
-		            alert.setHeaderText("Could not save data");
-		            alert.setContentText("Could not save data to file:\n" + file.getPath()+ "\n\n" + e);
-		
-		            alert.showAndWait();
-		        }
-		    }		    
+			// Reading XML from the file and unmarshalling.
+			PortfolioWrapper wrapper = (PortfolioWrapper) um.unmarshal(file);
+
+			criteriaList.clear();
+			criteriaList.addAll(wrapper.getCriterion());
+
+			icoList.clear();
+			icoList.addAll(wrapper.getIcos());
+
+			// Save the file path to the registry.
+			setFilePath(file);
+
+		} catch (Exception e) { // catches ANY exception
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error loading data");
+			alert.setHeaderText("Could not load data from file");
+			alert.setContentText("File is not valid.\n\n" + "Error Message:\n\n" + e);
+		    alert.showAndWait();
+		  }	    
+	}
+
+	/**
+	 * Saves the current person data to the specified file. 
+	 * @param file
+	 */
+	public void saveCriteriaDataToFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PortfolioWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			// Wrapping our criteria data.
+			PortfolioWrapper wrapper = new PortfolioWrapper();
+			wrapper.setCriterion(criteriaList);
+			wrapper.setIcos(icoList);
+			// Marshalling and saving XML to the file.
+			m.marshal(wrapper, file);
+
+			// Save the file path to the registry.
+			setFilePath(file);
+		} catch (Exception e) { // catches ANY exception
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Could not save data");
+			alert.setContentText("Could not save data to file:\n" + file.getPath() + "\n\n" + e);
+
+			alert.showAndWait();
+		}
+	}		    
 }
--- IcoRatingApp/src/icoRating/view/RootLayoutController.java
+++ IcoRatingApp/src/icoRating/view/RootLayoutController.java
@@ -1,10 +1,8 @@
 package icoRating.view;
 
-import java.beans.EventHandler;
 import java.io.File;
 import java.util.Optional;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -12,7 +10,6 @@ import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import icoRating.MainApp;
 
 /**
@@ -20,29 +17,31 @@ import icoRating.MainApp;
  * application layout containing a menu bar and space where other JavaFX
  * elements can be placed.
  * 
- * @author BeniW
+ * @author Benjamin Wyss
  */
 public class RootLayoutController {
 
-    // Reference to the main application
     private MainApp mainApp;
     private Stage primaryStage;
 
     /**
      * Is called by the main application to give a reference back to itself.
-     * 
      * @param mainApp
      */
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
     
-    public void setPrimaryStage (Stage ps) {
-    	this.primaryStage = ps;
+    /**
+     * Is called by the main application to give a reference to the Stage
+     * @param primaryStage	the current Stage
+     */
+    public void setPrimaryStage (Stage primaryStage) {
+    	this.primaryStage = primaryStage;
     }
 
     /**
-     * Creates an empty address book.
+     * Creates an empty portfolio.
      */
     @FXML
     private void handleNew() {
@@ -71,33 +70,49 @@ public class RootLayoutController {
     }
 
     /**
-     * Opens a FileChooser to let the user select an address book to load.
+     * Opens a FileChooser to let the user select a Portfolio to load.
+     * Asks to save changes at the current Portfolio
      */
     @FXML
     private void handleOpen() {
-    	handleSave();
-        FileChooser fileChooser = new FileChooser();
+    	Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("IcoRatingApp");
+        alert.setHeaderText("Save current Portfolio");
+        alert.setContentText("Do you want to save changes?");
+        ButtonType buttonTypeOne = new ButtonType("Save");
+        ButtonType buttonTypeTwo = new ButtonType("Discard Changes");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo, buttonTypeCancel);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeOne){
+            handleSave();
+            alert.close();
+        } else if (result.get() == buttonTypeCancel) {
+        	alert.close();
+        } else {
+        	FileChooser fileChooser = new FileChooser();
 
-        // Set extension filter
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
-                "XML files (*.xml)", "*.xml");
-        fileChooser.getExtensionFilters().add(extFilter);
+            // Set extension filter
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                    "XML files (*.xml)", "*.xml");
+            fileChooser.getExtensionFilters().add(extFilter);
 
-        // Show open file dialog
-        File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
+            // Show open file dialog
+            File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
 
-        if (file != null) {
-            mainApp.loadCriteriaDataFromFile(file);
+            if (file != null) {
+                mainApp.loadCriteriaDataFromFile(file);
+            }
         }
     }
 
     /**
-     * Saves the file to the person file that is currently open. If there is no
+     * Saves the file to the portfolio file that is currently open. If there is no
      * open file, the "save as" dialog is shown.
      */
     @FXML
     private void handleSave() {
-    	if (isInputValid()) {
+    	if (isPortfolioValid()) {
     	
     		File personFile = mainApp.getFilePath();
     		if (personFile != null) {
@@ -113,7 +128,7 @@ public class RootLayoutController {
      */
     @FXML
     private void handleSaveAs() {
-    	if (isInputValid()) {
+    	if (isPortfolioValid()) {
     	
     		FileChooser fileChooser = new FileChooser();
 
@@ -173,7 +188,13 @@ public class RootLayoutController {
         }
     }
     
-    private boolean isInputValid() {
+    /**
+     * A portfolio must consists of at least one Ico and one Criteria.
+     * Without that, loading existing data may throws a nullPointer.
+     * @return		true if the portfolio is sufficient
+     */
+    
+    private boolean isPortfolioValid() {
 		String errorMessage = "";
 		
 		if (mainApp.getIcoList().isEmpty()) {
@@ -192,7 +213,7 @@ public class RootLayoutController {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.initOwner(primaryStage);
 			alert.setTitle("No sufficient Portfolio.");
-			alert.setHeaderText("No sufficient Portfolio.");
+			alert.setHeaderText("No sufficient Portfolio. Could not save data.");
 			alert.setContentText(errorMessage);
 			
 			alert.showAndWait();
