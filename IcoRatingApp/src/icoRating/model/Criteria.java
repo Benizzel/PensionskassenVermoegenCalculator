168fbabc7dcc2aa13a5e8e78c7e6778eb0f2181e
beniw <beniw@LAPTOP-AKAFORD9.fritz.box>
2018-07-25 12:05:52 +0200

    Formating Code in Criteria

--- IcoRatingApp/src/icoRating/model/Criteria.java
+++ IcoRatingApp/src/icoRating/model/Criteria.java
@@ -12,23 +12,21 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
- * Model Class for a rating criteria of an ICO.
- * 
- * @author beniw
- *
+ * Model Class for a Criteria
+ * @author Benjamin Wyss
  */
-
 @XmlAccessorType(XmlAccessType.PROPERTY)
 public class Criteria {
 	
-//	private static int count = 0;
-	
 	@XmlAttribute
 	@XmlID
-	private String uuidString;
-	private UUID uuid;
-	
-	
+	/**
+	 * UUID must be stored as String because JAXB accepts only String as a XmlID
+	 * Do not delete variable and add XmlID to the method "getUuidAsString".
+	 * Elsewhere existing Portfolios cannot be loaded
+	 */
+	private String uuidString; //must be a variable because of JAXB accepts only String as an XmlID
+	private final UUID uuid;
 	private final StringProperty name;
 	private final StringProperty description;
 	private final StringProperty category;
@@ -37,10 +35,8 @@ public class Criteria {
 	/**
 	 * Default Constructor
 	 */
-	
 	public Criteria() {
 		this(null, null, null, Weight.MEDIUM);
-		//id=Integer.toString(count++);
 	}
 	
 	/**
@@ -50,7 +46,6 @@ public class Criteria {
 	 * @param category
 	 * @param weight
 	 */
-	
 	public Criteria(String name, String description, String category, Weight weight) {
 		this.name = new SimpleStringProperty(name);
 		this.description = new SimpleStringProperty(description);
@@ -58,84 +53,114 @@ public class Criteria {
 		this.setWeight(weight);
 		uuid = UUID.randomUUID();
 		uuidString = uuid.toString();
-		
 	}
 	
-	//holt sich das StringObject und gibt es als String zurück
+	/**
+	 * @return the UUID as a String
+	 */
+	public String getUuidAsString() {
+		return uuid.toString();
+	}
+	
+	/**
+	 * @return the UUID
+	 */
+	public UUID getUuid() {
+		return uuid;
+	}
+	
+	/**
+	 * @return name as a String
+	 */
 	public String getName() {
 		return name.get();
 	}
 	
-	//nimmt den String "name" und speichert es als StringObject in der variable "name" vom Object
+	/**
+	 * @param name
+	 */
 	public void setName (String name) {
 		this.name.set(name);
 	}
 	
-	//wird für das GUI benötigt - Ich nenne die Variable nameProperty, inhalt ist variable name
+	/**
+	 * 
+	 * @return name as a StringProperty
+	 */
 	public StringProperty nameProperty() {
 		return name;
 	}
 	
+	/**
+	 * @return description as a String
+	 */
 	public String getDescription() {
 		return description.get();
 	}
 	
+	/**
+	 * @param description
+	 */
 	public void setDescription (String description) {
 		this.description.set(description);
 	}
 	
+	/**
+	 * @return description as a StringProperty
+	 */
 	public StringProperty descriptionProperty() {
 		return description;
 	}
 	
+	/**
+	 * @return category as a String
+	 */
 	public String getCategory() {
 		return category.get();
 	}
 	
+	/**
+	 * @param category
+	 */
 	public void setCategory (String category) {
 		this.category.set(category);
 	}
 	
+	/**
+	 * @return category as a StringProperty
+	 */
 	public StringProperty categoryProperty() {
 		return category;
 	}
 
 	/**
-	 * @return the weight
+	 * @return the weight as enum Weight
 	 */
 	public Weight getWeight() {
 		return weight;
 	}
 	
+	/**
+	 * @return text of Weight as a StringProperty
+	 */
 	public StringProperty getWeightAsStringProperty() {
 		StringProperty weightString = new SimpleStringProperty();
 		weightString.setValue(weight.getText());
 		return weightString;
 	}
 	
+	/**
+	 * @return code of Weight as an int
+	 */
 	public int getWeightAsInt() {
 		int weightInt = weight.getCode();
 		return weightInt;
 	}
 
 	/**
-	 * @param weight the weight to set
+	 * @param enum weight to set
 	 */
 	public void setWeight(Weight weight) {
 		this.weight = weight;
-	}
-
-	/**
-	 * @return the id
-	 */
-	
-//	@XmlAttribute
-//	@XmlID
-	public String getUuidAsString() {
-		return uuidString;
-	}
-	
-	public UUID getUuid() {
-		return uuid;
 	}
 }
